803a3312bcb6478b932036bb698114615067ff0c
Woollame <bogdanul65@gmail.com>
2026-01-14 16:42:36 +0200

    dijkstra

--- funct.py
+++ funct.py
@@ -1,4 +1,5 @@
 from collections import deque
+import heapq
 
 #prezenta visited este doar pt eficenta mergand pe baza de hash-uri, aceasta poate fi scoasa si inlocuita doar de order daca nu cont eficenta
 
@@ -49,3 +50,52 @@ def bfs(graph_obj, start_node):
                 queue.append(neighbor)
 
     return order
+
+def dijkstra(graph_obj, start_node, target_node):
+    for i in range(2, len(graph_obj.edge_list), 3):
+        cost = graph_obj.edge_list[i]
+        if cost is None or cost == "None" or cost == "":
+            return "ERROR_COST"
+
+    distances = {node: float('inf') for node in graph_obj.node_list.keys()}
+    distances[start_node] = 0
+    predecessors = {node: None for node in graph_obj.node_list.keys()}
+    priority_queue = [(0, start_node)]
+    visited = set()
+
+    while priority_queue:
+        current_dist, u = heapq.heappop(priority_queue)  #heapq pt efficenta, radacina(index 0) mereu are cea mai mica val.
+
+        if u in visited:
+            continue
+        visited.add(u)
+
+        if u == target_node:
+            break
+
+        for i in range(0, len(graph_obj.edge_list), 3):
+            src = graph_obj.edge_list[i]
+            dest = graph_obj.edge_list[i+1]
+            weight = int(graph_obj.edge_list[i+2])
+
+            neighbor = None
+            if src == u: neighbor = dest
+            elif not graph_obj.isDirected and dest == u: neighbor = src
+
+            if neighbor is not None:
+                new_dist = current_dist + weight
+                if new_dist < distances[neighbor]:
+                    distances[neighbor] = new_dist
+                    predecessors[neighbor] = u
+                    heapq.heappush(priority_queue, (new_dist, neighbor))
+    path = []
+    curr = target_node
+    while curr is not None:
+        path.append(curr)
+        curr = predecessors[curr]
+    path.reverse() 
+
+    if path[0] != start_node:
+        return []
+        
+    return path
